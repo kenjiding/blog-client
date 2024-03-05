dd8c5962e73ab19b7ee95b6624f4b51a2560b634
kenjiding <872570730@qq.com>
2024-03-05 20:58:28 +1030

    feature: add dockerfile

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,5 +1,5 @@
 
 
 export const getHost = (path?: string) => {
-  return path ? `http://${process.env.NEXT_PUBLIC_SERVER_HOST}${path}` : `//${process.env.NEXT_PUBLIC_SERVER_HOST}`;
+  return path ? `//${process.env.NEXT_PUBLIC_SERVER_HOST}${path}` : `//${process.env.NEXT_PUBLIC_SERVER_HOST}`;
 }

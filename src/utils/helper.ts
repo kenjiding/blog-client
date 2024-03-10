24699fc2bea72d0d928489ddeec549d8ee51a5c7
kenjiding <872570730@qq.com>
2024-03-10 23:24:31 +1030

    feature: add jwt

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -25,5 +25,5 @@ export function catchError<T, U extends IFetchRes> (
 
 
 export const getHost = (path?: string) => {
-  return path ? `${path}` : ``;
+  return path ? `http://${process.env.NEXT_PUBLIC_SERVER_HOST}${path}` : `http://${process.env.NEXT_PUBLIC_SERVER_HOST}`;
 }
